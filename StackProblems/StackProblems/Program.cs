da34dad2232ff5e69d81093ba0467c6a8f5c9d5b
Anjali <anjali.9691@gmail.com>
2021-05-19 19:22:33 +0530

    Stacks - Assignment Problems 0 & 1

---
+++ StackProblems/StackProblems/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Assignment 0 - Delete Consecutive Words
+            List<string> inputStr = new List<string>() { "tom", "jerry", "jerry", "tom" };
+            int outputStr = DeleteConsecutiveWords(inputStr);
+            Console.WriteLine(outputStr);
+
+            //Assignment 1 - Minimum remove to make valid parantheses
+            string s = "(a(b(c)d)";
+            string resultStr = MinimumRemoveToMakeValidParantheses(s);
+            Console.WriteLine(resultStr);
+        }
+
+        //Assignement 0 - Delete Consecutive Words
+        public static int DeleteConsecutiveWords(List<string> s)
+        {
+            Stack<string> st = new Stack<string>();
+            int n = s.Count();
+            for (int i=0; i<n; i++)
+            {
+                if(st.Count() == 0)
+                {
+                    st.Push(s[i]);
+                }
+                else
+                {
+                    if(st.Peek() == s[i])
+                    {
+                        st.Pop();
+                    }
+                    else
+                    {
+                        st.Push(s[i]);
+                    }
+                }
+            }
+            return st.Count();
+        }
+
+        //Assignment 1 - Minimum remove to make valid parantheses
+        public static string MinimumRemoveToMakeValidParantheses(string s)
+        {
+            StringBuilder sb = new StringBuilder(s);
+            Stack<int> st = new Stack<int>();
+            for (int i=0; i<s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    st.Push(i);
+                }
+                else if (s[i] == ')')
+                {
+                    if(st.Count() > 0 && s[st.Peek()] == '(')
+                    {
+                        st.Pop();
+                    }
+                    else
+                    {
+                        sb.Remove(i, 1);
+                    }
+                }
+            }
+            while(st.Count() > 0)
+            {
+                sb.Remove(st.Peek(), 1);
+                st.Pop();
+            }
+            return sb.ToString();
+        }
+    }
+}
--- StackProblems/StackProblems/Properties/AssemblyInfo.cs
+++ StackProblems/StackProblems/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("StackProblems")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("StackProblems")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bc312504-6541-41db-a00f-32e2b58b38d2")]
+[assembly: Guid("16ea41b2-0328-4e63-8e4b-a920932dece1")]
 
 // Version information for an assembly consists of the following four values:
 //
